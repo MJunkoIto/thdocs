995449831e13f2d6d0b644bcfd5d09523ead0250
MJunkoIto <soutozen2018@gmail.com>
2024-12-03 06:51:34 +0900

    constant

---
+++ constant.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+  <body>
+    <?php
+    // 定数に値を代入する
+    const SHIPPING_FEE = 500;
+
+    // 中身を出力する
+    echo SHIPPING_FEE;
+   
+　　// クラス内に定数を定義する
+　　const SHIPPING_FEE = 800;
+    
+
+    // クラス内に定義された定数にアクセスする
+    echo SHIPPING_FEE;
+    ?>
+
+  </body>
+
+</html>
